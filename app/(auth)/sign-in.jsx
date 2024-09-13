f858c726c298819404d13900c143aae5ebc58801
Alon Krumer <alon1525@gmail.com>
2024-09-13 15:23:12 +0300

    init

---
+++ app/(auth)/sign-in.jsx
@@ -0,0 +1,71 @@
+import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {Link} from "expo-router"
+
+import { images } from "../../constants";
+import { CustomButton, FormField } from "../../components";
+
+export default function SignIn() {
+  const [isSubmitting, setSubmitting] = useState(false);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const submit = async () => {
+    if (form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+    }
+  };
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full flex justify-center min-h-[77vh] px-4 my-6 ">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[140px] h-[50px] -ml-4 -mt-1"
+          />
+          <View className="-mt-6">
+          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold ">
+            Log in to ScreenBuds
+          </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+          <View className="flex justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Signup
+            </Link>
+            
+          </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
---
+++ app/index.jsx
@@ -0,0 +1,45 @@
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import {Redirect, router} from "expo-router"
+import CustomButton from "../components/CustomButton";
+
+export default function App() {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
+          <Image
+            source={images.logo}
+            className="w-[250px] h-[90px]"
+            resizeMode="contain"
+          />
+          <Image source={images.cards} className="max-w--[38px} w-full h-[300px]" resizeMode="contain"/>
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Stop DoomScrolling{"\n"}
+              with{" "}
+              <Text className="text-secondary-200 text-secondary">ScreenBuds</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[190px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+            </View>
+            <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Challenge Yourself and Take Control {"\n"} over Your Social Media Screen Time
+          </Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => {router.push('/sign-in')}}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light"/>
+    </SafeAreaView>
+  );
+}
